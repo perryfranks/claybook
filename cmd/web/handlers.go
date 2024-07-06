aee670368ee2dcf912cbd72c3bde33a49b715708
perryfranks <30431929+perryfranks@users.noreply.github.com>
2024-07-07 02:36:26 +1000

    spell slots working. Next sorting spells by level

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"claybook.perryfranks.nerd/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +33,45 @@ func (app *application) spells(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) updateSpellSlot(w http.ResponseWriter, r *http.Request) {
 
-	w.Write([]byte("edit page"))
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// get the slot level
+	rawSlot := r.PostForm.Get("slotlevel")
+	slotLevel, err := strconv.Atoi(rawSlot)
+	if err == nil {
+		app.infoLog.Println("Slot Level: ", slotLevel)
+		dbSlots := app.spellbook.SpellSlots
+		for i, slot := range dbSlots {
+			if slot.Level == slotLevel {
+				dbSlots[i].Use()
+			}
+		}
+
+	}
+
+	// check for reset button
+	if r.PostForm.Get("reset") == "true" {
+		app.infoLog.Println("Resetting")
+		for i := range app.spellbook.SpellSlots {
+			// *slot.Reset()
+			app.spellbook.SpellSlots[i].Reset()
+		}
+	}
+
+	// update the spell slot
+	// get the corresponding spell slot
+
+	// save
+	// app.spellbook.("internal/data/spells.yaml")
+	models.SaveSpellbook("internal/data/spells.yaml", app.spellbook)
+	// http.Redirect(w, r, "/spells", http.StatusSeeOther)
+	data := app.newTemplateData(r)
+	app.render(w, http.StatusOK, "spellbook.tmpl", data)
+
 }
 
 func (app *application) edit(w http.ResponseWriter, r *http.Request) {
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,13 +11,17 @@ func (app *application) routes() http.Handler {
 
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 	router.HandlerFunc(http.MethodGet, "/spells", app.spells)
+	router.HandlerFunc(http.MethodPost, "/spells", app.updateSpellSlot)
 	router.HandlerFunc(http.MethodGet, "/edit", app.edit)
-	router.HandlerFunc(http.MethodPost, "/spells/update", app.updateSpellSlot)
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
